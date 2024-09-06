0c3d447d084b559b7dd71bf07ed71ef524b83d33
Rei Zhang <mauoser@outlook.com>
2024-09-07 06:36:21 +0900

    updated manifest

--- db/migrate/20240906171349_modify_locations_table.rb
+++
@@ -1,9 +0,0 @@
-class ModifyLocationsTable < ActiveRecord::Migration[7.1]
-  def change
-    # Remove the 'hours' column
-    remove_column :locations, :hours, :jsonb
-
-    # Add the 'photo' column
-    add_column :locations, :photo, :string
-  end
-end
