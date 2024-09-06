4dc37eaf38139b811d4e3853c314d61cf0cc1020
Rei Zhang <mauoser@outlook.com>
2024-09-07 05:51:34 +0900

    updated db key

--- config/application.rb
+++ config/application.rb
@@ -28,5 +28,7 @@ module AnimeLocationsApi
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+    config.assets.enabled = true
+    config.assets.compile = true
   end
 end
